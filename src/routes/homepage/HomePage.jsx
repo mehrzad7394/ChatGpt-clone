154e7a244dd084718792f2d96c741ed6b080ac08
Mehrzad Ardeshiri <mehrzad.ardeshiri@gmail.com>
2024-12-01 16:49:45 +0330

    css of background

--- src/routes/homepage/HomePage.jsx
+++ src/routes/homepage/HomePage.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className="flex items-center gap-24 h-full relative">
-      <img src="/orbital.png" alt="" className="absolute bottom-0 left-0 opacity-5 animate-rotateOrbital"/>
+      <img
+        src="/orbital.png"
+        alt=""
+        className="absolute bottom-0 left-0 opacity-5 animate-rotateOrbital"
+      />
       <div className="flex-1 flex flex-col justify-center items-center gap-4">
         <h1 className="text-3xl bg-gradient-to-r from-blue-500 to-green-100 bg-clip-text text-transparent uppercase">
           ChatGpt Clone
@@ -22,7 +26,14 @@ const HomePage = () => {
           Get Started
         </Link>
       </div>
-      <div className="flex-1"></div>
+      <div className="flex-1 flex items-center justify-center h-full">
+        <div className="flex  items-center bg-[#130e2d] rounded-[50px] h-[50%] relative">
+          <div className="size-full overflow-hidden absolute top-0 left-0 rounded-[50px]">
+            <div className="bg-bgbot opacity-5 size-full bg-auto animate-bgAnimation"></div>
+          </div>
+          <img src="./bot.png" alt="" className="w-full h-full object-contain animate-botAnimation"/>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        bgbot: "url('/bg.png')",
+      },
       keyframes: {
         rotateOrbital: {
           "0%": {
@@ -12,10 +15,28 @@ export default {
             transform: "rotate(60deg)",
           },
         },
+        botAnimation: {
+          "0%": {
+            transform: "scale(1) rotate(0deg)",
+          },
+          "100%": {
+            transform: "scale(1.1) rotate(-5deg)",
+          },
+        },
+        bgAnimation: {
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-50%)",
+          },
+        },
       },
-      animation:{
+      animation: {
         rotateOrbital: "rotateOrbital 100s linear infinite",
-      }
+        botAnimation: "botAnimation 3s ease-in-out infinite alternate",
+        bgAnimation: "bgAnimation 8s ease-in-out infinite alternate",
+      },
     },
   },
   plugins: [],
